7aa05740468d0b835767890d588a32f2f8695743
sj0919 <seungjinlee09@gmail.com>
2024-08-16 00:31:29 +0900

    fix CORS Error

--- src/main/java/com/stem/enterteample/domain/account/controller/AccountController.java
+++ src/main/java/com/stem/enterteample/domain/account/controller/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/accounts")
 @RequiredArgsConstructor
 public class AccountController {
