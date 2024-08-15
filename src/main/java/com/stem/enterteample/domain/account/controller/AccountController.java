249371146b5351c0b690e2748ca0c0d144579ea7
sj0919 <seungjinlee09@gmail.com>
2024-08-16 00:47:41 +0900

    test

--- src/main/java/com/stem/enterteample/domain/account/controller/AccountController.java
+++ src/main/java/com/stem/enterteample/domain/account/controller/AccountController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/account")
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
