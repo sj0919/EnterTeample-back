9faba36df159e85253036e259eea913201bd9e43
sj0919 <seungjinlee09@gmail.com>
2024-08-16 21:41:15 +0900

    test

--- src/main/java/com/stem/enterteample/domain/createteam/controller/CreateTeamController.java
+++ src/main/java/com/stem/enterteample/domain/createteam/controller/CreateTeamController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/createteams")
+@RequestMapping("/createteam")
 @RequiredArgsConstructor
 public class CreateTeamController {
 
